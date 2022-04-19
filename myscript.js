43c41fec7eaa9d39fa72d3f7f03762bf1f729d06
General Kelvin <asantekelvin133@gmail.com>
2022-04-19 17:16:02 +0000

    Javascript

---
+++ myscript.js
@@ -0,0 +1,19 @@
+var a = 45
+var b = 5
+
+console.log(
+    a*b
+);
+
+let p = 40
+let y = 20
+let z = 10
+
+if (y + z <= p) { 
+    console.log(
+        'true'
+    )
+}
+else {
+    console.log('not true')
+}
